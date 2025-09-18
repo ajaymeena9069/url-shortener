2a6d980056a3b02c5e9033cc9a0e49089d7b1027
ajaymeena9069 <amm62408@gmail.com>
2025-09-18 10:09:13 +0530

    fixing PORT error

--- app.js
+++ app.js
@@ -98,6 +98,6 @@ const server = createServer(async (req, res) => {
 
 const PORT = 3002; 
 
-server.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
